c945f9dc7fab478573852d2c737e384559272fd3
Afsha752 <afshachishti0102@gmail.com>
2026-08-11 19:03:13 +0530

    Add Navbar with React Router navigation and basic styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Login from './pages/Login'
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
